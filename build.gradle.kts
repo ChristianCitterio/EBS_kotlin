abeda0dc2ede404018fdcdb3a7c1e52962a89633
ChristianCitterio <christiancitterio18@gmail.com>
2024-07-15 19:17:04 +0200

    Delete Main.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    application
 }
 
 group = "org.example"
@@ -11,11 +12,18 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("net.dv8tion:JDA:5.0.0-beta.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(11)
+}
+
+application {
+    mainClass.set("org.example.Bot")
 }
---
+++ src/main/kotlin/Bot.kt
@@ -0,0 +1,36 @@
+package org.example
+
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import javax.security.auth.login.LoginException
+
+fun main(args: Array<String>) {
+    val token =
+        "MTIyNzY4NzUwNjY3MDEyOTE5Mg.GYlvYb.hScKDe-q2UDSbkOzUgRMclkiB6ABLULFeb_imo" // Replace with your bot token
+
+    try {
+        val jda = JDABuilder.createDefault(token)
+            .addEventListeners(Bot())
+            .build()
+        jda.awaitReady() // Blocking guarantees that JDA will be completely loaded.
+        println("Bot is ready!")
+    } catch (e: LoginException) {
+        e.printStackTrace()
+    } catch (e: InterruptedException) {
+        e.printStackTrace()
+    }
+}
+
+
+class Bot : ListenerAdapter() {
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        if (event.author.isBot) return
+
+        val message = event.message.contentRaw
+        if (message == "!ping") {
+            event.channel.sendMessage("Pong!").queue()
+        }
+    }
+}
