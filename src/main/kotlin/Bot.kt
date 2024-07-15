ac84bffbfebe7dface803afa36a5beae07cf4fa6
ChristianCitterio <christiancitterio18@gmail.com>
2024-07-16 01:29:15 +0200

    Add main function to main

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,28 +1,7 @@
 package org.example
 
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import javax.security.auth.login.LoginException
-
-fun main(args: Array<String>) {
-    val token =
-        "MTIyNzY4NzUwNjY3MDEyOTE5Mg.GYlvYb.hScKDe-q2UDSbkOzUgRMclkiB6ABLULFeb_imo" // Replace with your bot token
-
-    try {
-        val jda = JDABuilder.createDefault(token)
-            .addEventListeners(Bot())
-            .build()
-        jda.awaitReady() // Blocking guarantees that JDA will be completely loaded.
-        println("Bot is ready!")
-    } catch (e: LoginException) {
-        e.printStackTrace()
-    } catch (e: InterruptedException) {
-        e.printStackTrace()
-    }
-}
-
 
 class Bot : ListenerAdapter() {
     override fun onMessageReceived(event: MessageReceivedEvent) {
@@ -34,3 +13,4 @@ class Bot : ListenerAdapter() {
         }
     }
 }
+
