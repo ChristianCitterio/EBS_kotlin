3764b43e00409b84b69179e0427a211d3e1f4c4e
ChristianCitterio <christiancitterio18@gmail.com>
2024-07-29 23:15:50 +0200

    Add MessageListener.kt

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # EBS_kotlin
+
+:heavy_check_mark: 3.0 Należy stworzyć aplikację kliencką w Kotlinie we frameworku Ktor, 
+    która pozwala na przesyłanie wiadomości na platformę Discord
+:heavy_check_mark: 3.5 Aplikacja jest w stanie odbierać wiadomości użytkowników z
+    platformy Discord skierowane do aplikacji (bota)
+:heavy_check_mark: 4.0 Zwróci listę kategorii na określone żądanie użytkownika
+:heavy_multiplication_x: 4.5 Zwróci listę produktów wg żądanej kategorii
+:heavy_multiplication_x: 5.0 Aplikacja obsłuży dodatkowo jedną z platform: Slack, Messenfer, Webex, Skype, Discrod
--- src/main/kotlin/MessageListener.kt
+++ src/main/kotlin/MessageListener.kt
@@ -3,13 +3,21 @@ package org.example
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class Bot : ListenerAdapter() {
+class MessageListener : ListenerAdapter() {
+
     override fun onMessageReceived(event: MessageReceivedEvent) {
         if (event.author.isBot) return
 
         val message = event.message.contentRaw
         if (message == "!ping") {
             event.channel.sendMessage("Pong!").queue()
+        }
+
+        if (message == "!categories") {
+            val categories = listOf("Coocking", "Ingredients", "Utensils")
+            event.channel.sendMessage(
+                "Here are the categories:\n" + categories.joinToString(separator=",\n")
+            ).queue()
         }
     }
 }
